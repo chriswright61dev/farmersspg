bf9c366d138d4ec3058ecff9f6297b7917b6e7e2
Chris Wright <chriswrightdev@gmail.com>
2021-01-19 22:11:57 +0000

    fetch venue json

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -5,7 +7,65 @@ import Venue from "./components/Venue/Venue";
 import Events from "./components/Events/Events";
 import Drinks from "./components/Drinks/Drinks";
 import Foods from "./components/Foods/Foods";
+
 function App() {
+  const [venueState, setVenueState] = useState({
+    loading: false,
+    theVenueData: null,
+  });
+
+  useEffect(() => {
+    setVenueState({ loading: true });
+    const venueApiUrl = "http://levenshulmelife.com/drupal9/venue-farmers-api/";
+    fetch(venueApiUrl)
+      .then((res) => res.json())
+      .then((loadedData) => {
+        setVenueState({
+          loading: false,
+          theVenueData: loadedData[0],
+          // the api returns an array with one object in it
+        });
+      });
+  }, [setVenueState]);
+
+  console.log("venueState", venueState);
+
+  field_venue_description: "Lounge and Sports Bar"
+  ​​
+  venue_address_1: "254 Burnage Lane"
+  ​​
+  venue_address_2: "Burnage"
+  ​​
+  venue_address_3: "Manchester"
+  ​​
+  venue_email: "farmersburnage@gmail.com"
+  ​​
+  venue_id: "2"
+  ​​
+  venue_image_large_480: "http://levenshulmelife.com/drupal9/sites/default/files/style…ge/public/2021-01/farmers-burnage-exterior.jpg?itok=ZEPzswTb "
+  ​​
+  venue_image_medium_220: "http://levenshulmelife.com/http://levenshulmelife.com//drupal9/sites/default/files/2021-01/farmers-burnage-exterior.jpg"
+  ​​
+  venue_image_original: "http://levenshulmelife.com//drupal9/sites/default/files/2021-01/farmers-burnage-exterior.jpg"
+  ​​
+  venue_image_thumbnail: "http://levenshulmelife.com//drupal9/sites/default/files/styl…il/public/2021-01/farmers-burnage-exterior.jpg?itok=z5_ATBqH"
+  ​​
+  venue_image_wide: "http://levenshulmelife.com/http://levenshulmelife.com//drupal9/sites/default/files/2021-01/farmers-burnage-exterior.jpg"
+  ​​
+  venue_logo: "http://levenshulmelife.com/drupal9/sites/default/files/2021-01/farmers-arms-burnage-logo-white-no-bg.svg"
+  ​​
+  venue_name: "Farmers Arms"
+  ​​
+  venue_postcode: "M19 1EN"
+  ​​
+  venue_telephone: "0161 432 6441"
+
+
+
+
+
+
+
   return (
     <div className="App">
       <Header />
