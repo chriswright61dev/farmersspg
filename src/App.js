65a66859164503002c13f63ed506df044818d52b
Chris Wright <chriswrightdev@gmail.com>
2021-01-19 20:52:39 +0000

    css for basic outline

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 function App() {
-  return "app";
-  // <div className="App">
-  //   <div className="header">header</div>
-  //   <div className="container">
-  //     <div className="info-column">info</div>
-  //     <div className="events-column">events</div>
-  //     <div className="drinks-column">drinks</div>
-  //     <div className="food-column">food</div>
-  //   </div>
-  //   <div className="footer">footer</div>
-  // </div>
+  return (
+    <div className="App">
+      <Header />
+      <div className="container">
+        <div className="info-column">info</div>
+        <div className="events-column">events</div>
+        <div className="drinks-column">drinks</div>
+        <div className="food-column">food</div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
