c11f1716033496cf8c90a5c4d326eaaa4fc944c6
Chris Wright <chriswrightdev@gmail.com>
2021-01-19 21:37:45 +0000

    venue component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Venue from "./components/Venue/Venue";
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="container">
-        <div className="info-column">info</div>
+        <div className="info-column">
+          <Venue />
+        </div>
         <div className="events-column">events</div>
         <div className="drinks-column">drinks</div>
         <div className="food-column">food</div>
---
+++ src/components/Venue/Venue.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Venue.css";
+function Venue() {
+  return (
+    <div className="venue">
+      venue image here
+      <div className="venue-info">venue address etc</div>
+    </div>
+  );
+}
+
+export default Venue;
